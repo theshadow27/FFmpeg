3b9ac8f5c4a943e55bd2fb78c27a16988e515bfd
Ronald S. Bultje <rsbultje@gmail.com>
2013-02-02 09:33:23 -0800

    bit_depth_template: don't depend on dsputil.h.

--- libavcodec/bit_depth_template.c
+++ libavcodec/bit_depth_template.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "dsputil.h"
+#include "libavutil/intreadwrite.h"
 
 #ifndef BIT_DEPTH
 #define BIT_DEPTH 8
