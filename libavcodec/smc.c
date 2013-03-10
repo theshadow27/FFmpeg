87689937da2dd46d06732e1fdaf7d3776cf0b934
Clément Bœsch <ubitux@gmail.com>
2013-03-10 21:52:43 +0100

    smc: resolve conflicts

--- libavcodec/smc.c
+++ libavcodec/smc.c
@@ -435,19 +435,7 @@ static int smc_decode_frame(AVCodecContext *avctx,
 
     bytestream2_init(&s->gb, buf, buf_size);
 
-<<<<<<< HEAD
-    s->frame.reference = 3;
-    s->frame.buffer_hints = FF_BUFFER_HINTS_VALID | FF_BUFFER_HINTS_PRESERVE |
-                            FF_BUFFER_HINTS_REUSABLE | FF_BUFFER_HINTS_READABLE;
-    if (avctx->reget_buffer(avctx, &s->frame)) {
-||||||| merged common ancestors
-    s->frame.reference = 1;
-    s->frame.buffer_hints = FF_BUFFER_HINTS_VALID | FF_BUFFER_HINTS_PRESERVE |
-                            FF_BUFFER_HINTS_REUSABLE | FF_BUFFER_HINTS_READABLE;
-    if (avctx->reget_buffer(avctx, &s->frame)) {
-=======
     if ((ret = ff_reget_buffer(avctx, &s->frame)) < 0) {
->>>>>>> 759001c534287a96dc96d1e274665feb7059145d
         av_log(s->avctx, AV_LOG_ERROR, "reget_buffer() failed\n");
         return ret;
     }
