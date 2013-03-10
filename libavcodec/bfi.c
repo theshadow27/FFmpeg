d4a6b9fb45503d2062c13908a403599c8850595a
Hendrik Leppkes <h.leppkes@gmail.com>
2013-03-10 18:26:46 +0100

    bfi: fix compilation

--- libavcodec/bfi.c
+++ libavcodec/bfi.c
@@ -41,7 +41,6 @@ static av_cold int bfi_decode_init(AVCodecContext *avctx)
 {
     BFIContext *bfi = avctx->priv_data;
     avctx->pix_fmt  = AV_PIX_FMT_PAL8;
-    avcodec_get_frame_defaults(&bfi->frame);
     bfi->dst        = av_mallocz(avctx->width * avctx->height);
     return 0;
 }
