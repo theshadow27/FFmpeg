d00fda8a9d1db64871f4ba46992aa7bc491a4543
Michael Niedermayer <michaelni@gmx.at>
2013-03-11 14:32:23 +0100

    cdxl: fix fate
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/cdxl.c
+++ libavcodec/cdxl.c
@@ -35,7 +35,6 @@
 
 typedef struct {
     AVCodecContext *avctx;
-    AVFrame        frame;
     int            bpp;
     int            format;
     int            padded_bits;
@@ -118,7 +117,7 @@ static void cdxl_decode_rgb(CDXLVideoContext *c, AVFrame *frame)
 {
     uint32_t *new_palette = (uint32_t *)frame->data[1];
 
-    memset(c->frame.data[1], 0, AVPALETTE_SIZE);
+    memset(frame->data[1], 0, AVPALETTE_SIZE);
     import_palette(c, new_palette);
     import_format(c, frame->linesize[0], frame->data[0]);
 }
